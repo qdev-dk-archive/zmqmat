5c154eb0f0bed8151c68b58805fadddfc74d595e
Anders Jellinggaard <anders.jel@gmail.com>
2013-05-05 08:57:09 +0200

    Cleanup object where appropriate.

--- +zmq/wait.m
+++ +zmq/wait.m
@@ -14,29 +14,24 @@ function ready = wait(sockets, timeout)
         socketptrs = [socketptrs socket.get_raw_ptr()];
     end
     array = calllib('zmqmat', 'zmqmat_array_new', numel(socketptrs));
-    free = @()calllib('zmqmat', 'zmqmat_array_free', array);
-    try
-        for socketptr = socketptrs
-            calllib('zmqmat', 'zmqmat_array_insert', array, socketptr);
-        end
-
-        id = tic();
-        r = 0;
-        while true
-            time_left = timeout - toc(id)*1000;
-            tim = max(min(time_left, 200), 0);
-            r = calllib('zmqmat', 'zmqmat_wait', array, tim);
-            if r == -1
-                zmq.internal.throw_zmq_error();
-            elseif r > 0 || time_left < 0
-                break;
-            end
-            drawnow();
-        end
-        ready = r >= 1;
-    catch err
-        free();
-        rethrow(err);
+    cleanup = onCleanup(@()calllib('zmqmat', 'zmqmat_array_free', array));
+    
+    for socketptr = socketptrs
+        calllib('zmqmat', 'zmqmat_array_insert', array, socketptr);
     end
-    free();
+
+    id = tic();
+    r = 0;
+    while true
+        time_left = timeout - toc(id)*1000;
+        tim = max(min(time_left, 200), 0);
+        r = calllib('zmqmat', 'zmqmat_wait', array, tim);
+        if r == -1
+            zmq.internal.throw_zmq_error();
+        elseif r > 0 || time_left < 0
+            break;
+        end
+        drawnow();
+    end
+    ready = r >= 1;
 end
